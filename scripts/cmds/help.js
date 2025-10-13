2832cfdf372a8611cdc8063f56004732dba21e54
fakereactionx210-hub <fakereactionx210@gmail.com>
2025-10-13 19:38:30 +0600

    Update help.js
    Signed-off-by: fakereactionx210-hub <fakereactionx210@gmail.com>

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "〲٭⃝✨⃝YOUR 卝 চুন্নি ⃝✨⃝٭";
+const doNotDelete = "〲MAYBE NX ";
 /** 
 * @author NTKhang
 * @author: do not delete it
